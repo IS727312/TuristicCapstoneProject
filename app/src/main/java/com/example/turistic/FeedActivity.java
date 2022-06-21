3162945b3cbd9738129e332027c937840ae2bc0f
Miguel Méndez <is727312@iteso.mx>
2022-06-21 11:04:46 -0700

    Desgined the Compose Layout

--- app/src/main/java/com/example/turistic/FeedActivity.java
+++ app/src/main/java/com/example/turistic/FeedActivity.java
@@ -2,13 +2,21 @@ package com.example.turistic;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 
+import com.example.turistic.fragments.FeedFragment;
+import com.example.turistic.fragments.ProfileFragment;
+import com.example.turistic.fragments.ComposeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.parse.ParseUser;
 
 import java.util.Objects;
@@ -16,7 +24,9 @@ import java.util.Objects;
 public class FeedActivity extends AppCompatActivity {
 
     public static final String TAG = "FeedActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +37,8 @@ public class FeedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_feed);
 
         ImageButton btnFeedLogOut = findViewById(R.id.btnFeedLogOut);
+        ImageButton btnFeedSearchPost = findViewById(R.id.btnFeedSearchPost);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
 
         btnFeedLogOut.setOnClickListener(v -> ParseUser.logOutInBackground(e -> {
             if (e != null){
@@ -40,5 +52,33 @@ public class FeedActivity extends AppCompatActivity {
                 startActivity(i);
             }
         }));
+
+        btnFeedSearchPost.setOnClickListener(v -> {
+            Intent i = new Intent(FeedActivity.this, SearchActivity.class);
+            startActivity(i);
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment fragment;
+            switch (item.getItemId()){
+                case R.id.action_profile:
+                    Toast.makeText(FeedActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                    fragment = new ProfileFragment();
+                    break;
+                case R.id.action_compose:
+                    Toast.makeText(FeedActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                    fragment = new ComposeFragment();
+                    break;
+                case R.id.action_feed:
+                default:
+                    Toast.makeText(FeedActivity.this, "Home", Toast.LENGTH_SHORT).show();
+                    fragment = new FeedFragment();
+                    break;
+            }
+            fragmentManager.beginTransaction().replace(R.id.flFeedContainer, fragment).commit();
+            return true;
+        });
+        //Default selection for the BottomNavigationView
+        bottomNavigationView.setSelectedItemId(R.id.action_feed);
     }
 }
---
+++ app/src/main/java/com/example/turistic/fragments/FeedFragment.java
@@ -0,0 +1,33 @@
+package com.example.turistic.fragments;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.turistic.R;
+
+public class FeedFragment extends Fragment {
+
+    public static final String TAG = "FeedFragment";
+
+    public FeedFragment(){
+        // Required empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
