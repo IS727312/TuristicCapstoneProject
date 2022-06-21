33ac8ecaa3d90befa20f5e11a496f24e3988ef00
Miguel Méndez <is727312@iteso.mx>
2022-06-21 13:50:39 -0700

    Can Take or Submit pictures
    The user can either take a picture with the camera intent or upload a picture from there library and post it.
    
    Test:
    
    - Click the compose button on the BottomNavigation
    - Fill the required fields
    - CLick Submit
    - Should return to Feed compose

--- app/src/main/java/com/example/turistic/fragments/ComposeFragment.java
+++ app/src/main/java/com/example/turistic/fragments/ComposeFragment.java
@@ -1,20 +1,53 @@
 package com.example.turistic.fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.turistic.BitmapScaler;
+import com.example.turistic.FeedActivity;
 import com.example.turistic.R;
+import com.example.turistic.models.Post;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class ComposeFragment extends Fragment {
 
     public static final String TAG = "ComposeFragment";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    public static final int SELECT_PICTURE = 200;
+    private EditText etComposeTitle;
+    private ImageView ivComposePictureToPost;
+    private EditText etComposeCaption;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
 
     public ComposeFragment() {
         // Required empty public constructor
@@ -30,5 +63,169 @@ public class ComposeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Log.i(TAG, "COMPOSE FRAGMENT");
+        Button btnComposeSubmitPicture = view.findViewById(R.id.btnComposeSubmitPicture);
+        Button btnComposeSubmitPost = view.findViewById(R.id.btnComposeSubmitPost);
+        Button btnComposeTakePicture = view.findViewById(R.id.btnComposeTakePicture);
+        etComposeTitle = view.findViewById(R.id.etComposeTitle);
+        etComposeCaption = view.findViewById(R.id.etComposeCaption);
+        ivComposePictureToPost = view.findViewById(R.id.ivComposePictureToPost);
+
+        btnComposeTakePicture.setOnClickListener(v -> launchCamera());
+
+        btnComposeSubmitPost.setOnClickListener(v -> {
+            String caption = etComposeCaption.getText().toString();
+            String title = etComposeTitle.getText().toString();
+            if(caption.isEmpty() || title.isEmpty()){
+                Toast.makeText(getContext(), "Empty fields", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(photoFile == null || ivComposePictureToPost.getDrawable() == null){
+                Toast.makeText(getContext(), "There is no photo", Toast.LENGTH_SHORT).show();
+            }
+            ParseUser currentUser = ParseUser.getCurrentUser();
+            savePosts(caption, title, currentUser, photoFile);
+            Intent i = new Intent(getContext(), FeedActivity.class);
+            startActivity(i);
+        });
+
+        btnComposeSubmitPicture.setOnClickListener(v -> {
+            Log.i(TAG, "Upload pictures");
+            Toast.makeText(getContext(), "SELECT PICTURES", Toast.LENGTH_SHORT).show();
+            imageChooser();
+        });
     }
+
+    private void savePosts(String caption, String title, ParseUser currentUser, File photoFile) {
+        Post post = new Post();
+        post.setTitle(title);
+        post.setPicture(new ParseFile(photoFile));
+        post.setCaption(caption);
+        post.setOwner(currentUser);
+        post.saveInBackground(e -> {
+            if(e != null){
+                Log.e(TAG, "Error while saving the post: ", e);
+                return;
+            }
+            Log.i(TAG, "Save successful");
+            etComposeCaption.setText("");
+            etComposeTitle.setText("");
+            ivComposePictureToPost.setImageResource(0);
+        });
+    }
+
+    private void launchCamera(){
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(requireContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(requireContext().getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+
+        return new File(mediaStorageDir.getPath() + File.separator + fileName);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                //All the code below helps to resize the image
+                //
+                Uri takenPhotoUri = Uri.fromFile(getPhotoFileUri(photoFileName));
+                // by this point we have the camera photo on disk
+                Bitmap rawTakenImage = BitmapFactory.decodeFile(takenPhotoUri.getPath());
+                // See BitmapScaler.java: https://gist.github.com/nesquena/3885707fd3773c09f1bb
+                Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(rawTakenImage, 400);
+
+                // Configure byte output stream
+                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+                // Compress the image further
+                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
+                // Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
+                File resizedFile = getPhotoFileUri(photoFileName + "_resized");
+                try {
+                    resizedFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(resizedFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                // Write the bytes of the bitmap to file
+                try {
+                    assert fos != null;
+                    fos.write(bytes.toByteArray());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                ivComposePictureToPost.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        if (requestCode == SELECT_PICTURE) {
+            // compare the resultCode with the
+            // SELECT_PICTURE constant
+            if (resultCode == RESULT_OK) {
+                // Get the url of the image from data
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    // update the preview image in the layout
+                    ivComposePictureToPost.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+
+    private void imageChooser() {
+
+        // create an instance of the
+        // intent of the type image
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+
+        // pass the constant to compare it
+        // with the returned requestCode
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+
 }
--- app/src/main/java/com/example/turistic/models/Post.java
+++ app/src/main/java/com/example/turistic/models/Post.java
@@ -1,7 +1,13 @@
 package com.example.turistic.models;
 
+import android.util.Log;
+
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.Date;
 
 @ParseClassName("Post")
 public class Post extends ParseObject {
@@ -11,5 +17,52 @@ public class Post extends ParseObject {
     public static final String KEY_PICTURE = "picture";
     public static final String KEY_OWNER = "owner";
     public static final String KEY_LIKES = "likes";
+    public static final String KEY_TITLE = "title";
+
+    public String getCaption() {return KEY_CAPTION; }
+    public void setCaption(String caption) {put(KEY_CAPTION, caption); }
+    public ParseFile getPicture() {return getParseFile(KEY_PICTURE); }
+    public void  setPicture(ParseFile picture) {put(KEY_PICTURE, picture); }
+    public ParseUser getOwner() {return  getParseUser(KEY_OWNER); }
+    public void setOwner(ParseUser owner) {put(KEY_OWNER, owner); }
+    public String getTitle() {return KEY_TITLE; }
+    public void setTitle(String title) {put(KEY_TITLE, title); }
+    public int getLikes() {return getInt(KEY_LIKES); }
+    public void increaeLikes() {put(KEY_LIKES, getLikes() + 1);}
+
+    public String getRelativeTimeAgo(Date createdAt) {
+        final int SECOND_MILLIS = 1000;
+        final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+        final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+        final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+        try {
+            long time = createdAt.getTime();
+            long now = System.currentTimeMillis();
+
+            final long diff = now - time;
+            if (diff < MINUTE_MILLIS) {
+                return "just now";
+            } else if (diff < 2 * MINUTE_MILLIS) {
+                return "a minute ago";
+            } else if (diff < 50 * MINUTE_MILLIS) {
+                return diff / MINUTE_MILLIS + " m";
+            } else if (diff < 90 * MINUTE_MILLIS) {
+                return "an hour ago";
+            } else if (diff < 24 * HOUR_MILLIS) {
+                return diff / HOUR_MILLIS + " h";
+            } else if (diff < 48 * HOUR_MILLIS) {
+                return "yesterday";
+            } else {
+                return diff / DAY_MILLIS + " d";
+            }
+        } catch (Exception e) {
+            Log.i(TAG, "getRelativeTimeAgo failed", e);
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
 
 }
