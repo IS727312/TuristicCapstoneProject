980376f0de8f85e894e12dfc95803313e99b93f5
Miguel Méndez <is727312@iteso.mx>
2022-07-15 11:18:48 -0700

    Added users details page requirements
    Now, depending on the profile mode of the user you are trying to see it will take you, or not, to their profile page.
    Test:
    -Login
    -Look up a user you are not friends with(rivers)
    -Click her card
    -Toast of error appears
    -Look for a person ypu are friends with(cosmonaut)
    -UserDetailsActivityShowsUp
    
    https://pxl.cl/27ThD

--- app/src/main/java/com/example/turistic/adapters/UserAdapter.java
+++ app/src/main/java/com/example/turistic/adapters/UserAdapter.java
@@ -1,5 +1,10 @@
 package com.example.turistic.adapters;
 
+import static com.example.turistic.enumerations.PrivacyMode.FOLLOWERS_ONLY;
+import static com.example.turistic.enumerations.PrivacyMode.FRIENDS_ONLY;
+import static com.example.turistic.enumerations.PrivacyMode.PRIVATE;
+import static com.example.turistic.enumerations.PrivacyMode.PUBLIC;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,17 +28,22 @@ import com.parse.ParseUser;
 
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import es.dmoral.toasty.Toasty;
 
 public class UserAdapter  extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
 
     public static final String sTAG = "UserAdapter";
     private Context mContext;
     private List<ParseUser> mUsers;
+    private ParseUser mCurrentUser;
 
     public UserAdapter(Context context, List<ParseUser> users){
         this.mContext = context;
         this.mUsers = users;
+        this.mCurrentUser = ParseUser.getCurrentUser();
     }
 
     @NonNull
@@ -60,6 +71,7 @@ public class UserAdapter  extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
         private TextView mTvLastName;
         private TextView mTvUsername;
 
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             mTvUsername = itemView.findViewById(R.id.tvCardUsername);
@@ -74,11 +86,26 @@ public class UserAdapter  extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
             int position = getAdapterPosition();
             if(position != RecyclerView.NO_POSITION){
                 ParseUser user = mUsers.get(position);
-                Intent intent = new Intent(mContext, UserDetailsActivity.class);
-                // serialize the movie using parceler, use its short name as a key
-                intent.putExtra(ParseUser.class.getSimpleName(), Parcels.wrap(user));
-                mContext.startActivity(intent);
-                Log.i(sTAG, "Post Clicked");
+                switch (user.getInt("profileMode")){
+                    case PUBLIC:
+                        goToUserDetails(user);
+                        break;
+                    case FOLLOWERS_ONLY:
+                        if(isFollowing(user)){
+                            goToUserDetails(user);
+                        }else{
+                            Toasty.error(itemView.getContext(), "You are not following this user", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case FRIENDS_ONLY:
+                        if(isFollowing(user) && isFollowedBy(user)){
+                            goToUserDetails(user);
+                        }else {
+                            Toasty.error(itemView.getContext(), "You are not friends with this user", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                }
+
             }
         }
 
@@ -92,6 +119,37 @@ public class UserAdapter  extends RecyclerView.Adapter<UserAdapter.ViewHolder>{
             if(profileImage != null){
                 Glide.with(mContext).load(profileImage.getUrl()).into(mIvProfilePicture);
             }
+        }
+
+        public void goToUserDetails(ParseUser user){
+            Intent intent = new Intent(mContext, UserDetailsActivity.class);
+            // serialize the movie using parceler, use its short name as a key
+            intent.putExtra(ParseUser.class.getSimpleName(), Parcels.wrap(user));
+            mContext.startActivity(intent);
+        }
+
+        private boolean isFollowing(ParseUser owner) {
+            ArrayList<ParseUser> followingList = (ArrayList) mCurrentUser.get("following");
+            if(followingList != null){
+                for(ParseUser user: followingList){
+                    if(user.getObjectId().equals(owner.getObjectId())){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private boolean isFollowedBy(ParseUser owner) {
+            ArrayList<ParseUser> postOwnerFollowingList = (ArrayList) owner.get("following");
+            if(postOwnerFollowingList != null){
+                for(ParseUser user: postOwnerFollowingList){
+                    if(mCurrentUser.getObjectId().equals(user.getObjectId())){
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
